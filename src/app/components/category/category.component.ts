5bcf3cfd575dd736e4f5da64f5373465e6b9ddd7
mariam <mariemsafwat20@gmail.com>
2024-08-15 01:42:11 +0300

    Build Categories And Category Component

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [NavbarComponent, NgxPaginationModule, CommonModule],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css'
+})
+
+export class CategoryComponent {
+  categoryArray: Array<any> = [
+        {
+          "id":	1,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	2,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	3,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	4,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	5,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	6,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	7,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	8,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	5,
+          "img":	"assets/imgs/.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	6,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	7,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry ",
+          "bookAuthor":	"J. K. Rowling",
+        },
+        {
+          "id":	8,
+          "img":	"assets/imgs/book.jpg",
+          "bookName":	"Harry Potter",
+          "bookAuthor":	"J. K. ",
+        }
+  ]
+  p: number = 1;
+
+  constructor(private router:Router){}
+
+  singlebook(productId: number){
+    this.router.navigate(['/singleBook', productId])
+  }
+
+  author(Id: number){
+    this.router.navigate(['/author', Id])
+  }
+}
