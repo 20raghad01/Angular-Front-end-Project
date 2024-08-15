4e01263bb1d1f5971f8a6a26710bf652224241c8
Mariam Safwat <100482871+mariemsafwat20@users.noreply.github.com>
2024-08-16 00:51:46 +0300

    Router Authors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,18 @@ import { StatusComponent } from './components/user\'s status/status/status.compo
 import { HomeComponent } from './components/home/home/home.component';
 import { BooksComponent } from './components/books/books/books.component';
 import { SinglebookComponent } from './components/singlebook/singlebook.component';
+import { AuthorsComponent } from './components/authors/authors.component';
+import { AuthorComponent } from './components/author/author.component';
 
 export const routes: Routes = [
     { path: '', component:HomeComponent},
     { path: 'all', component:StatusComponent },
+
     { path: 'books', component:BooksComponent },
     { path: 'singleBook/:id', component:SinglebookComponent },
+
+    { path: 'authors', component:AuthorsComponent },
+    { path: 'author/:id', component:AuthorComponent },
+
     { path: '**', component:NotfoundComponent },
 ];
